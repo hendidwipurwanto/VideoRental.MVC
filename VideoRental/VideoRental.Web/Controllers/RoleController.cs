88899ceff22cb8e199908d1214ec87f69bc520ad
hendidwipurwanto <hendi.dp@gmail.com>
2017-10-15 19:12:50 +0700

    Add RoleController and View

---
+++ VideoRental/VideoRental.Web/Controllers/RoleController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using VideoRental.ViewModel.Views;
+using VideoRental.Web.DbContext;
+
+namespace VideoRental.Web.Controllers
+{
+    public class RoleController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            var list = new List<RoleView>();
+            var request = db.Roles;
+            foreach (var item in request)
+            {
+                var model = new RoleView()
+                {
+                    Id = item.Id,
+                    Name = item.Name
+                };
+
+                list.Add(model);
+            }
+
+            return View(list);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ VideoRental/ViewModel/Views/RoleView.cs
@@ -0,0 +1,10 @@
+﻿using VideoRental.Common.BaseClass;
+
+namespace VideoRental.ViewModel.Views
+{
+    public class RoleView : BaseView
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
